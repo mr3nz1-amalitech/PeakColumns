98a2757d927378f67dece487d5b458676608114e
Murenzi Paterne <murenzi419@gmail.com>
2024-06-16 10:36:12 +0200

    Caught errors Added comments

--- src/Main.java
+++ src/Main.java
@@ -1,96 +1,112 @@
-import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
+    //    Get user matrix dimensions from the user's input, and return an array with two values where at index 0 we have rows, and index 1 cols
     static int[] getDims(Scanner scanner) {
-        String dimLn = scanner.nextLine().toLowerCase();
-        String[] dimsStrArr = dimLn.substring(dimLn.lastIndexOf("x") + 5, dimLn.length()).trim().split(",");
-
-        int[] dimsArr = new int[2];
-
-        for (int i = 0; i < dimsStrArr.length; i++) {
-            dimsArr[i] = Integer.parseInt(dimsStrArr[i]);
+        try {
+            String dimLn = scanner.nextLine().toLowerCase();
+            String[] dimsStrArr = dimLn.substring(dimLn.lastIndexOf("x") + 5).trim().split(",");
+            int[] dimsArr = new int[2];
+            for (int i = 0; i < dimsStrArr.length; i++) {
+                dimsArr[i] = Integer.parseInt(dimsStrArr[i]);
+            }
+            return dimsArr;
+        } catch (Exception err) {
+            throw new RuntimeException(err);
         }
-        return dimsArr;
     }
 
+    //    Process the matrix values from the input and return a 2 dimensional of numbers representing the matrix
     static int[][] getMat(int[] dimsArr, Scanner scanner) {
-        int[][] arr = new int[dimsArr[0]][dimsArr[1]];
+        try {
 
-        for (int i = 0; i < dimsArr[0]; i++) {
-            String ln = scanner.nextLine();
-            String[] rowStrArr = ln.split(" ");
+            int[][] arr = new int[dimsArr[0]][dimsArr[1]];
 
-            int[] rowVals = new int[0];
+            for (int i = 0; i < dimsArr[0]; i++) {
+                String ln = scanner.nextLine();
+                String[] rowStrArr = ln.split(" ");
 
-            for (int j = 0; j < rowStrArr.length; j++) {
-                if (rowStrArr[j].isBlank()) {
-                    continue;
+                int[] rowVals = new int[0];
+
+                for (String s : rowStrArr) {
+                    if (s.isBlank()) {
+                        continue;
+                    }
+
+                    int[] newArr = new int[rowVals.length + 1];
+
+                    System.arraycopy(rowVals, 0, newArr, 0, rowVals.length);
+
+                    newArr[rowVals.length] = Integer.parseInt(s);
+                    rowVals = newArr;
                 }
 
-                int[] newArr = new int[rowVals.length + 1];
-
-                for (int k = 0; k < rowVals.length; k++) {
-                    newArr[k] = rowVals[k];
-                }
-
-                newArr[rowVals.length] = Integer.parseInt(rowStrArr[j]);
-                rowVals = newArr;
+                arr[i] = rowVals;
             }
-
-            arr[i] = rowVals;
+            return arr;
+        } catch (Exception err) {
+            throw new RuntimeException(err);
         }
-
-        return arr;
     }
 
+    //    iterate over rows, then cols, then rows to find peak values
     static int[] getPeakCols(int[][] mat) {
-        int[] peakCols = new int[0];
+        try {
+            int[] peakCols = new int[0];
 
-        for (int i = 0; i < mat.length; i++) {
-            int maxInRow = 0;
+            for (int i = 0; i < mat.length; i++) {
+                int maxInRow = 0;
 
-            for (int j = 0; j < mat[i].length; j++) {
-                if (mat[i][j] > maxInRow) {
-                    maxInRow = mat[i][j];
-                }
-            }
-
-            for (int j = 0; j < mat[i].length; j++) {
-                int minInCol = mat[i][j];
-
-                for (int k = 0; k < mat.length; k++) {
-                    if (mat[k][j] < minInCol) {
-                        minInCol = mat[k][j];
+                for (int j = 0; j < mat[i].length; j++) {
+                    if (mat[i][j] > maxInRow) {
+                        maxInRow = mat[i][j];
                     }
                 }
 
-                if (minInCol == maxInRow) {
-                    int[] newArr = new int[peakCols.length + 1];
-                    Arrays.fill(newArr, minInCol);
-                    newArr[peakCols.length] = minInCol;
-                    peakCols = newArr;
+                for (int j = 0; j < mat[i].length; j++) {
+                    int minInCol = mat[i][j];
 
-                    System.out.println("(" + (i + 1) + "," + (j + 1) + ") = " + minInCol);
+                    for (int[] ints : mat) {
+                        if (ints[j] < minInCol) {
+                            minInCol = ints[j];
+                        }
+                    }
+
+                    if (minInCol == maxInRow) {
+                        int[] newArr = new int[peakCols.length + 1];
+                        Arrays.fill(newArr, minInCol);
+                        newArr[peakCols.length] = minInCol;
+                        peakCols = newArr;
+
+                        System.out.println("(" + (i + 1) + "," + (j + 1) + ") = " + minInCol);
+                    }
                 }
             }
-        }
 
-        return peakCols;
+            return peakCols;
+        } catch (Exception err) {
+            throw new RuntimeException(err);
+        }
     }
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print(
-                "Sample input: \nMatrix A: 3,3\n" +
-                        "12  2  4\n" +
-                        "17 10  1\n" +
-                        "92 80 79\n");
-        int[] dimsArr = getDims(scanner);
-        int[][] mat = getMat(dimsArr, scanner);
-
-        System.out.println("\nOutput\n");
-        int[] peakCols = getPeakCols(mat);
+        try {
+            Scanner scanner = new Scanner(System.in);
+            System.out.print(
+                    """
+                            Sample input:\s
+                            Matrix A: 3,3
+                            12  2  4
+                            17 10  1
+                            92 80 79
+                            """);
+            int[] dimsArr = getDims(scanner);
+            int[][] mat = getMat(dimsArr, scanner);
+            System.out.println("\nOutput\n");
+            int[] peakCols = getPeakCols(mat);
+        } catch (RuntimeException err) {
+            System.out.println("Error: input error. \nEnsure your input is properly formatted");
+        }
     }
 }
